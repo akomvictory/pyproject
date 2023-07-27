b2c925d333b390686de737a7946023846a4be7e9
akom <akom.victory@gmail.com>
2023-07-27 17:12:59 +0100

    new commit3

---
+++ die3.py
@@ -0,0 +1,56 @@
+import random
+
+class Player:
+    def __init__(self):
+        self.roll = None
+        self.rollsCount = 0
+        self.atStartup = True
+        self.winner = False
+        self.loser = False
+
+    def rollDice(self):
+        if self.atStartup:
+            self.roll = self._getDiceValues()
+            self.atStartup = False
+            self.rollsCount = 1
+        else:
+            self.roll = self._getDiceValues()
+            self.rollsCount += 1
+            if sum(self.roll) in (2, 3, 12):
+                self.loser = True
+            elif sum(self.roll) in (7, 11):
+                self.winner = True
+
+    def getRollsCount(self):
+        return self.rollsCount
+
+    def isWinner(self):
+        return self.winner
+
+    def isLoser(self):
+        return self.loser
+
+    def getNumberOfRolls(self):
+        return self.rollsCount
+
+    def _getDiceValues(self):
+        return (random.randint(1, 6), random.randint(1, 6))
+
+def playOneGame():
+    player = Player()
+    while not player.isWinner() and not player.isLoser():
+        player.rollDice()
+        print(f"Roll {player.getRollsCount()}: {player.roll}")
+    if player.isWinner():
+        print("Congratulations! You won!")
+    else:
+        print("Sorry, you lost.")
+
+def playManyGames(num_games):
+    for _ in range(num_games):
+        print("Playing a new game...")
+        playOneGame()
+        print("=" * 30)
+
+# Example usage:
+playManyGames(3)  # Play 3 games
